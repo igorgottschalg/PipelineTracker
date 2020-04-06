a0ce3b03b12eabf2d0bb7e9cc58784e512080df2
Igor Gottschalg <igor@gottschalg.mobi>
2020-04-06 08:32:20 -0300

    Project init

---
+++ functions/index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+const firestore = admin.firestore().collection("pipelines");
+
+const checkAuth = req => {
+    const auth = {login: "gottschalg", password: "clnadm9"};
+    const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
+    const [login, password] = Buffer.from(b64auth, "base64")
+        .toString()
+        .split(":");
+    return (
+        login && password && login === auth.login && password === auth.password
+    );
+};
+
+const add = pipelineInfo => {
+    const {project, pipeline, step, status, datetime, job, branch} = pipelineInfo;
+    return firestore.doc(project).doc(branch).add({
+        pipeline,
+        job,
+        step,
+        status,
+        datetime
+    });
+};
+
+const app = (request, response) => {
+    if (!checkAuth(request)) {
+        response.set("WWW-Authenticate", 'Basic realm="401"');
+        response.status(401).send("Authentication required.");
+    }
+    add(request.query)
+        .then(() => {
+            return response.status(200).json(request.query);
+        })
+        .catch(e => {
+            return response.status(500).send(e);
+        });
+};
+
+exports.api = functions.https.onRequest(app);
